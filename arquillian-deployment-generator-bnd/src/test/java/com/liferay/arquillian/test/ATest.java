9c7acd8ac525ad9a913176ab00bc06c87755507b
Cristina González Castellano <liferay.es@gmail.com>
2015-01-12 15:29:01 +0100

    Auto SF

--- arquillian-deployment-generator-bnd/src/test/java/com/liferay/arquillian/test/ATest.java
+++ arquillian-deployment-generator-bnd/src/test/java/com/liferay/arquillian/test/ATest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -30,4 +30,5 @@ public class ATest {
         if (1==1)
             throw new BundleException("test");
 	}
+
 }
